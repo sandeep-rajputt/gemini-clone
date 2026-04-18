53aa5e0e164084c9e7d1f18abd6428cbd9fd2e12
sandeep-rajputt <sndp.rajpuut@gmail.com>
2026-04-18 09:51:13 +0530

    feat(gemini): add API key validation and support multiple API keys with random selection

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,29 +1,33 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+const API_KEYS_STORAGE = import.meta.env.VITE_GEMINI_API_KEY;
+if (!API_KEYS_STORAGE) {
+  throw new Error("GEMINI_API_KEY is not defined");
+}
 
-const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash-lite",
-  config: {
-    systemInstruction: [
-      `You are Gemini. From now on, always respond in valid Markdown (.md) format.  
+const API_KEYS = API_KEYS_STORAGE.split(",");
+
+const runChat = async (prompt, history = []) => {
+  const GEMINI_API_KEY = API_KEYS[Math.floor(Math.random() * API_KEYS.length)];
+  console.log(GEMINI_API_KEY);
+
+  const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-3-flash-preview",
+    config: {
+      systemInstruction: [
+        `You are Gemini. From now on, always respond in valid Markdown (.md) format.  
 - Use Markdown headings (#, ##, ###) for structure.  
 - Use bullet points (- or *) and numbered lists (1., 2.) where needed.  
 - Format code examples inside fenced code blocks with the correct language tag (\`\`\`javascript, \`\`\`python, etc.).  
 - Always keep the response ready to be directly saved as a .md file without requiring further edits.  
 - Do not output plain text outside Markdown formatting.   
 `,
-    ],
-  },
-  generationConfig: {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-  },
-});
+      ],
+    },
+  });
 
-const runChat = async (prompt, history = []) => {
   try {
     const chat = model.startChat({
       history: history.map((msg) => ({
